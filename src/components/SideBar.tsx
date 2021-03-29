f41f9461cc1c2786f5dec869ca935b66ddd557b4
Dikezeko Oliveira <dikezeko.oliveira@firgun.com.br>
2021-03-29 17:10:50 -0300

    Challenge completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,41 @@
-export function SideBar() {
-  // Complete aqui
+import { useState } from "react";
+
+import { Button } from "./Button";
+
+interface GenreResponse {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreResponse[];
+  setSelectedGenreId: (id: number) => void;
+}
+
+export function SideBar({ genres, setSelectedGenreId }: SideBarProps) {
+  const [currentGenreId, setCurrentGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setCurrentGenreId(id);
+    setSelectedGenreId(id);
+  }
+  
+  return(
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+  
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={currentGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
